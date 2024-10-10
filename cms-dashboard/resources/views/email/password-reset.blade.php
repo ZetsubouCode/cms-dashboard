c72d11aa5e61d190e9203237a86eaabb283196be
ZetsubouCode <kignatius80@gmail.com>
2024-10-10 07:20:32 +0700

    Add email content for reset password

---
+++ cms-dashboard/resources/views/email/password-reset.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Reset Code</title>
+</head>
+<body>
+    <h1>Your Password Reset Code</h1>
+    <p>Your verification code is: <strong>{{ $token??'' }}</strong></p>
+</body>
+</html>
